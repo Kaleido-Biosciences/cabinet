35ddf9e7c851d811a291d4d34610e9926e7c8193
QpYujin <yujin@qpair.io>
2020-03-10 11:11:43 -0400

    Changed to retrieve by checksum and activityname for draft

--- src/main/java/com/kaleido/web/rest/PlateMapResource.java
+++ src/main/java/com/kaleido/web/rest/PlateMapResource.java
@@ -4,7 +4,7 @@ import com.kaleido.domain.PlateMap;
 import com.kaleido.domain.enumeration.Status;
 import com.kaleido.repository.PlateMapRepository;
 import com.kaleido.repository.search.PlateMapSearchRepository;
-import com.kaleido.service.dto.DataDTO;
+//import com.kaleido.service.dto.DataDTO;
 import com.kaleido.service.dto.PlateMapDTO;
 import com.kaleido.web.rest.errors.BadRequestAlertException;
 
@@ -218,27 +218,28 @@ public class PlateMapResource {
     }
     
     @GetMapping("/plate-maps/data/draft/{activityName}")
-    public ResponseEntity<@Valid DataDTO> getDraftPlateMapDataByActivityName(@PathVariable String activityName) {
+    public ResponseEntity<@Valid PlateMap> getDraftPlateMapDataByActivityName(@PathVariable String activityName) {
         log.debug("REST request to get PlateMap draft data: {}", activityName);
         ExampleMatcher matcher = ExampleMatcher.matching().withIgnoreNullValues();
         PlateMap plateMap = new PlateMap();
         plateMap.setActivityName(activityName);
         plateMap.setStatus(Status.DRAFT);
         Example<@Valid PlateMap> plateMapQuery = Example.of(plateMap, matcher);
-        //List<@Valid PlateMap> results = plateMapRepository.findAll(plateMapQuery);
-        Optional<@Valid DataDTO> results = plateMapRepository.findDataByActivityName(activityName);
-        //return ResponseEntity.ok(results);
+        Optional<@Valid PlateMap> results = plateMapRepository.findOne(plateMapQuery);
         return ResponseUtil.wrapOrNotFound(results);
     }
     
-    @PostMapping("/plate-maps/data/completed")
-    public ResponseEntity<List<@Valid PlateMap>> getDraftPlateMapDataByActivityNameAndTimestamp(@Valid @RequestBody PlateMap plateMap) {
-    	log.debug("REST request to get PlateMap completed data: {}", plateMap);
+    @GetMapping("/plate-maps/data/completed/{activityName}/{checksum}")
+    public ResponseEntity<@Valid PlateMap> getDraftPlateMapDataByActivityNameAndTimestamp(@PathVariable String activityName, @PathVariable String checksum) {
+    	log.debug("REST request to get PlateMap completed data: {}", activityName, ", ", checksum);
         ExampleMatcher matcher = ExampleMatcher.matching().withIgnoreNullValues();
+        PlateMap plateMap = new PlateMap();
         plateMap.setStatus(Status.COMPLETED);
+        plateMap.setActivityName(activityName);
+        plateMap.setChecksum(checksum);
         Example<@Valid PlateMap> plateMapQuery = Example.of(plateMap, matcher);
-        List<@Valid PlateMap> results = plateMapRepository.findAll(plateMapQuery);
-        return ResponseEntity.ok(results);
+        Optional<@Valid PlateMap> results = plateMapRepository.findOne(plateMapQuery);
+        return ResponseUtil.wrapOrNotFound(results);
     }
     
     /**
