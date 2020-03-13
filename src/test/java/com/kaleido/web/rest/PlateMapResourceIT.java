d07df91bf0eeedae1d1a4c363680f3f6e9926201
QpYujin <yujin@qpair.io>
2020-03-13 15:05:44 -0400

    Wrong branch changes

--- src/test/java/com/kaleido/web/rest/PlateMapResourceIT.java
+++ src/test/java/com/kaleido/web/rest/PlateMapResourceIT.java
@@ -278,8 +278,7 @@ public class PlateMapResourceIT {
         restPlateMapMockMvc.perform(put("/api/plate-maps")
             .contentType(TestUtil.APPLICATION_JSON_UTF8)
             .content(TestUtil.convertObjectToJsonBytes(plateMap)))
-            .andExpect(status().isBadRequest())
-            .andReturn();
+            .andExpect(status().isBadRequest());
 
         // Validate the PlateMap in the database
         List<PlateMap> plateMapList = plateMapRepository.findAll();
